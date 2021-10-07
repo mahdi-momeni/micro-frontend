fa8dfb5d76492157dd7ea9738b1e419839e2247e
Mahdi <mahdimomeni6@gmail.com>
2021-10-07 17:54:36 +0330

    initial commit

---
+++ container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Marketing from "./components/Marketing";
+
+export default () => {
+    return (
+        <>
+            <h1>container</h1>
+            <hr/>
+            <Marketing/>
+        </>
+    )
+}
